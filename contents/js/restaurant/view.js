722f653c510291155f96cd50d516a31fff459311
Tristan Wagar <trwa7487@colorado.edu>
2015-03-13 20:19:03 -0600

    view done

--- contents/js/restaurant/view.js
+++ contents/js/restaurant/view.js
@@ -1,11 +1,75 @@
+
 var RestaurantView = React.createClass({
+
+    getInitialState: function() {
+        return {restaurant: null}
+    },
 
     render: function() {
 
-        return (
-            <div>
-                TODO
-            </div>
-        )
-    }
+        if (this.state.restaurant){
+
+            return ( 
+                <div className="restaurantView examplegrid" >
+                    <div className="twelve columns" >
+                        
+
+                        <div className="row" >
+
+                            <p className="restaurantName name twelve columns" >
+                                {this.state.restaurant.name}
+                            </p>
+                        </div>
+
+
+
+                        <div className="row" >
+                            <h6 className="three columns" >
+                                    Address: 
+                            </h6>
+
+                            <h6 className="nine columns" >
+                                {this.state.restaurant.full_address}
+                            </h6>
+
+                        </div>
+
+                        <div className="row" >
+
+                            <h6 className="three columns">
+                                Category: 
+                            </h6>
+
+                            <h6 className="nine columns">
+                                {this.state.restaurant.categories}
+                            </h6>
+
+                        </div>
+
+                        <div className="row" >
+
+                            <h6 className="three columns">
+                                Stars: 
+                            </h6>
+
+                            <h6 className="nine columns">
+                                {this.state.restaurant.stars}
+                            </h6>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+            )
+
+        }else{
+
+            return (
+                <h2>Click an item to see details</h2>
+            )
+        }
+  }
 })
